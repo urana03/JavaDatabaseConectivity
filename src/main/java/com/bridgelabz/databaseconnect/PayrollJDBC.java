447715424ffd35cb9ecb6eecd8775077a466a9bb
urana03 <uhackninja@gmail.com>
2022-12-18 11:46:16 +0530

    Add: Load the driver and connect to database then show connection

---
+++ src/main/java/com/bridgelabz/databaseconnect/PayrollJDBC.java
@@ -0,0 +1,42 @@
+package com.bridgelabz.databaseconnect;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+public class PayrollJDBC {
+
+	public static void main(String args[]) {
+		 String jdbcURL = "jdbc:mysql://localhost:3306/payroll?useSSL=false";
+		 String userName = "root";
+		 String password = "root";
+		 Connection connection;
+		 try {
+			 Class.forName("com.mysql.jdbc.Driver");
+			 System.out.println("Driver Loaded!");
+		 }
+		 catch(ClassNotFoundException e){
+			 throw new IllegalStateException("Cannot find the driver in the classpath!",e);
+		 }
+		 
+		 listDrivers(); 
+		 try {
+			 System.out.println("Connecting to database: "+jdbcURL);
+			 connection = DriverManager.getConnection(jdbcURL,userName,password);
+			 System.out.println("Connection is successfull!!"+connection);
+		 }
+		 catch(Exception e) {
+			 e.printStackTrace();
+		 }
+	}
+
+	private static void listDrivers() {
+		Enumeration<Driver> driverList = DriverManager.getDrivers();
+		while(driverList.hasMoreElements()) {
+			Driver driverClass = (Driver) driverList.nextElement();
+			System.out.println(" "+driverClass.getClass().getName());
+		}
+		
+	}
+}
