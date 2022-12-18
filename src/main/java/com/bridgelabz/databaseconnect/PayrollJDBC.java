65b87c57b276e0333285eab4d80a338ed472f671
urana03 <uhackninja@gmail.com>
2022-12-18 15:09:02 +0530

    Add: Retrieve and print the data of the table in the given database

--- src/main/java/com/bridgelabz/databaseconnect/PayrollJDBC.java
+++ src/main/java/com/bridgelabz/databaseconnect/PayrollJDBC.java
@@ -3,40 +3,34 @@ package com.bridgelabz.databaseconnect;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Enumeration;
+import java.sql.Statement;
 
 public class PayrollJDBC {
 
-	public static void main(String args[]) {
-		 String jdbcURL = "jdbc:mysql://localhost:3306/payroll?useSSL=false";
-		 String userName = "root";
-		 String password = "root";
-		 Connection connection;
-		 try {
-			 Class.forName("com.mysql.jdbc.Driver");
-			 System.out.println("Driver Loaded!");
-		 }
-		 catch(ClassNotFoundException e){
-			 throw new IllegalStateException("Cannot find the driver in the classpath!",e);
-		 }
-		 
-		 listDrivers(); 
-		 try {
-			 System.out.println("Connecting to database: "+jdbcURL);
-			 connection = DriverManager.getConnection(jdbcURL,userName,password);
-			 System.out.println("Connection is successfull!!"+connection);
-		 }
-		 catch(Exception e) {
-			 e.printStackTrace();
-		 }
-	}
+	 static final String jdbcURL = "jdbc:mysql://localhost:3306/payroll?useSSL=false";
+	 static final String userName = "root";
+	 static final String password = "root";
+	 static final String query = "SELECT id, name, salary, start FROM employee_payroll";
+	
+	public static void main(String args[]) throws SQLException {
 
-	private static void listDrivers() {
-		Enumeration<Driver> driverList = DriverManager.getDrivers();
-		while(driverList.hasMoreElements()) {
-			Driver driverClass = (Driver) driverList.nextElement();
-			System.out.println(" "+driverClass.getClass().getName());
-		}
-		
+		Connection connection = DriverManager.getConnection(jdbcURL, userName, password);
+		         Statement state = connection.createStatement();
+		         ResultSet result = state.executeQuery(query);
+				try{		      
+		         while(result.next()){
+		            //Display values
+		            System.out.print("ID: " + result.getInt("id"));
+		            System.out.print(", Name: " + result.getString("name"));
+		            System.out.print(", Salary: " + result.getDouble("salary"));
+		            System.out.println(", Start: " + result.getDate("start"));
+		         }
+		      } catch (SQLException e) {
+		         new SQLException("An error occured!");
+		      } 
+			connection.close();
 	}
 }
