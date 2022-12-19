55563b807b35156cd4d465b73bf7c52d29d2c884
urana03 <uhackninja@gmail.com>
2022-12-19 14:35:51 +0530

    Add: Retrieve the data from a particular date range to a date

--- src/main/java/com/bridgelabz/databaseconnect/PayrollJDBC.java
+++ src/main/java/com/bridgelabz/databaseconnect/PayrollJDBC.java
@@ -14,24 +14,18 @@ public class PayrollJDBC {
 	 static final String jdbcURL = "jdbc:mysql://localhost:3306/payroll?useSSL=false";
 	 static final String userName = "root";
 	 static final String password = "root";
-	 static final String query = "SELECT id, name, salary, start FROM employee_payroll";
+	 static final String query = "SELECT id, name, salary, start FROM employee_payroll WHERE start BETWEEN CAST('2018-01-01' AS DATE) AND DATE(NOW())";
 	
 	public static void main(String args[]) throws SQLException {
 
 		Connection connection = DriverManager.getConnection(jdbcURL, userName, password);
 		        try{		      
-		        	String sqlUpdate = "UPDATE employee_payroll  SET salary = ? WHERE id = ?";
-		        	PreparedStatement pstmt = connection.prepareStatement(sqlUpdate);
-		                 double salary = 4000000;
-		                 int id = 2;
-		                 pstmt.setDouble(1,salary);
-		                 pstmt.setInt(2,id);
-		                 pstmt.executeUpdate();
-		                 ResultSet result = pstmt.executeQuery(query);
+		        	Statement state = connection.createStatement();
+		                 ResultSet result = state.executeQuery(query);
 		        	
 		         while(result.next()){
 		            //Display values
-		            System.out.print("ID: " + result.getInt("id"));
+		            System.out.print("Id: " + result.getInt("id"));
 		            System.out.print(", Name: " + result.getString("name"));
 		            System.out.print(", Salary: " + result.getDouble("salary"));
 		            System.out.println(", Start: " + result.getDate("start"));
